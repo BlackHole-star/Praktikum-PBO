c4c598418bb531822c06e0f7cf9a67bcd622e7c4
Angga Syaputra <77041738+BlackHole-star@users.noreply.github.com>
2023-03-15 16:39:36 +0700

    Add files via upload

---
+++ posttest/MTimSepakbola.java
@@ -0,0 +1,49 @@
+/*
+Nama File 	: MTimSepakbola.java
+Pembuat		: Angga Syaputra - 24060121140137
+Tanggal		: Rabu, 15 Maret 2023
+Deskripsi 	: Berupa file operasi titik
+*/
+
+class MTimSepakbola {
+  public static void main(String[] args) {
+    // TODO: Make an instance called 'liverpool' -> use constructor which you have made before
+    TimSepakbola liverpool;
+	liverpool = new TimSepakbola("liverpool");
+	
+    // TODO: Make an instance called 'manchesterUnited' -> use constructor which you have made before
+    TimSepakbola manchesterUnited;
+	manchesterUnited = new TimSepakbola("manchesterUnited");
+
+    // TODO: call startTandingBola()
+	liverpool.startTandingBola(manchesterUnited);
+	
+		// Assume that liverpool wins against manchesterUnited 7 - 0
+    for (int i = 0; i < 7; i++) {
+  		liverpool.goal();
+
+			// TODO: Replace [SKOR LIVERPOOL] and [SKOR MANCHESTERUNITED] with the correct METHOD
+			// Code below should produce the output "Skor sementara: Liverpool 7 - 0 Manchester United"
+			System.out.println("Skor sementara: " + liverpool.nama + " " + liverpool.getskor + " - " + manchesterUnited.getskor + " " + manchesterUnited.nama);
+    }
+
+		// TODO: call endTandingBola()
+		liverpool.endTandingBola(manchesterUnited);
+  }
+}
+
+/* 
+Expected output:
+
+Pertandingan antara Liverpool dan Manchester United dimulai!
+
+Skor sementara: Liverpool 1 - 0 Manchester United
+Skor sementara: Liverpool 2 - 0 Manchester United
+Skor sementara: Liverpool 3 - 0 Manchester United
+Skor sementara: Liverpool 4 - 0 Manchester United
+Skor sementara: Liverpool 5 - 0 Manchester United
+Skor sementara: Liverpool 6 - 0 Manchester United
+Skor sementara: Liverpool 7 - 0 Manchester United
+
+Pertandingan antara Manchester United dan Liverpool selesai!
+*/
