1f2927bde7af2ec19bf8cf4bae86891f1aaec514
Angga Syaputra <77041738+BlackHole-star@users.noreply.github.com>
2023-05-26 17:28:22 +0700

    Update BangunDatarGeneric.java

--- Praktikum 8/BangunDatarGeneric.java
+++ Praktikum 8/BangunDatarGeneric.java
@@ -4,14 +4,14 @@ Pembuat		: Angga Syaputra - 24060121140137
 Deskripsi 	: kelas konstruksi generic untuk BangunDatar 
 */
 
-public class BangunDatarGeneric<T extends BangunDatar>{
-	private T bangunDatar;
+public class BangunDatarGeneric<F extends BangunDatar>{
+	private F bangunDatar;
 	
-	public void set(T tipeBangunDatar){
+	public void set(F tipeBangunDatar){
 		bangunDatar = tipeBangunDatar;
 	}
 	
-	public T get(){
+	public F get(){
 		return bangunDatar;
 	}
 	
@@ -19,3 +19,6 @@ public class BangunDatarGeneric<T extends BangunDatar>{
 		return bangunDatar.hitungKeliling();
 	}
 }
+
+// JIka Mengubah karakter 'T' menjadi 'T2' atau karakter lain di dalam kelas BangunDatarGeneric, kode tersebut akan tetap berjalan tanpa masalah. 
+//Karakter yang digunakan hanya sebagai nama variabel saja
