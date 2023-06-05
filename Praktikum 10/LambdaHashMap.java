875cb097e0e862ba487a07a6e057b2a1c92db46f
Angga Syaputra <77041738+BlackHole-star@users.noreply.github.com>
2023-06-05 21:10:29 +0700

    Update LambdaHashMap.java

--- Praktikum 10/LambdaHashMap.java
+++ Praktikum 10/LambdaHashMap.java
@@ -11,6 +11,10 @@ public class LambdaHashMap{
     public static void main(String[] args) {
         Map<String, String> mahasiswaMap = new HashMap<>();
         mahasiswaMap.put("24060121140137", "Angga Syaputra");
+        mahasiswaMap.put("24060121140125", "Raditya Ilham Hastoro");
+        mahasiswaMap.put("24060121140095", "Wildan Adji");
+        mahasiswaMap.put("24060121130037", "Muhammad Naufal Arkan");
+        mahasiswaMap.put("24060121120001", "Muhammad Alif Muckhlis");
 
         // Menggunakan ekspresi lambda untuk menampilkan key dan value
         mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " + nim + ", Nama: " + nama));
